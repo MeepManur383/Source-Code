29a4027683579218a16cc1c7eb9efea0053e8afb
ashwi <ashwi@10.0.0.79>
2019-03-02 20:25:39 -0500

    Two players can play the game and enter illegal and legal moves without penalty.

--- Tic Tac Toe Game/src/com/ashwin/comsci/ttt/PlayTTT.java
+++ Tic Tac Toe Game/src/com/ashwin/comsci/ttt/PlayTTT.java
@@ -1,21 +1,39 @@
 package com.ashwin.comsci.ttt;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class PlayTTT {
 	public static void main(String[] args) {
+		Scanner kboard = new Scanner(System.in);
 		 TTTBoard gameBoard = new TTTBoard();
-		 gameBoard.printGameState();
-		 
-		 System.out.println(Arrays.toString(gameBoard.getPossibleMoves("X")));
-		 
-		 gameBoard.makeLegalMove("X", 2, 1);
-		 gameBoard.printGameState();
-		 
-		 System.out.println(Arrays.toString(gameBoard.getPossibleMoves("X")));
-		 
-		 gameBoard.makeLegalMove("O", 2, 1);
-		 gameBoard.printGameState();
-		 System.out.println(gameBoard.gameResult());
+		 for(int counter = 0; counter < 9; counter++) {
+			 String player;
+			 if(counter % 2 == 0) {
+				 player = "X";
+			 }
+			 else {
+				 player = "O";
+			 }
+			 System.out.println("It's " + player + "'s turn!");
+			 System.out.print("Enter the row number of your desired move: ");
+			 int rowN = kboard.nextInt() -1;
+			 System.out.print("Enter the column number of your desired move: ");
+			 int colN = kboard.nextInt() - 1;
+			 while(gameBoard.makeLegalMove(player, rowN, colN) == false) {
+				 System.out.println("You must enter a legal move.");
+				 System.out.print("Enter the row number of a different move: ");
+				 rowN = kboard.nextInt() - 1;
+				 System.out.print("Enter the column number of a different move: ");
+				 colN = kboard.nextInt() - 1;
+			 }
+			 gameBoard.printGameState();
+			 
+			 String result = gameBoard.gameResult();
+			 System.out.println(result);
+			 if (!result.equals(TTTBoard.THE_GAME_IS_INCOMPLETE)) {
+				 break;
+			 }
+		 }
+		 kboard.close();
 	}
 }
