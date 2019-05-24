07061d1f51db514fed371285c758f3658aa67057
Ashwin Manur <ashwin.manur@gmail.com>
2019-05-24 19:33:38 -0400

    Adding back missing chapters.

---
+++ AP CSA HW/src/ashwin/manur/APCSA/hw/Chapter2/Exercises/Solutions/PrintFace.java
@@ -0,0 +1,16 @@
+package ashwin.manur.APCSA.hw.Chapter2.Exercises.Solutions;
+
+// Chapter 2 Question 9
+
+public class PrintFace
+{
+  public static void main(String[] args)
+  {
+    System.out.println("   xxxxx   ");
+    System.out.println("  x     x  ");
+    System.out.println("((  o o  ))");
+    System.out.println("  |  V  |  ");
+    System.out.println("  | === |  ");
+    System.out.println("   -----   ");
+  }
+}
---
+++ AP CSA HW/src/ashwin/manur/APCSA/hw/Chapter2/Hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package ashwin.manur.APCSA.hw.Chapter2.Hello;
+
+/**
+ * Displays a "Hello World!" message on the screen 
+ */
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello, World!");
+  }
+}
